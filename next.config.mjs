63e1b65173417a5165b3979e1ae0d5fb000f4743
asriewahyuni <asrie.wahyuni@gmail.com>
2026-08-26 20:24:15 +0700

    chore: allow any external domains in next/image to prevent hostname errors

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,12 @@ const nextConfig = {
       },
     ];
   },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '**' },
+      { protocol: 'http', hostname: '**' }
+    ]
+  }
 };
 
 export default nextConfig;
